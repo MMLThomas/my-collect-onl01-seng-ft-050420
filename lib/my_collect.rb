c8886dc07e6734ac157eaf6702cffaac5a108353
michael thomas <michael.thomas2014@gmail.com>
2020-05-11 00:34:30 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,10 +4,8 @@ def my_collect(array)
   i = 0 
   collection = []
   while i < array.length
-  binding.pry
-    collection << yield(array[i])
-    i++
-  end
+    {collection << yield(array[i])
+    i++}
   collection
 end
 
